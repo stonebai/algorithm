11454b65b9f56e68a40f0d89aa3527f0f04619dc
stonebai <dairystone@gmail.com>
2016-03-30 17:44:20 -0400

    [46. Permutations][Accepted]committed by LeetCode Extension

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -4,9 +4,9 @@ public class Solution {
     }
     
     private List<List<Integer>> permute(int[] nums, int start) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
         if(start==nums.length-1) {
-            List<Integer> list = new ArrayList<Integer>();
+            List<Integer> list = new LinkedList<Integer>();
             list.add(nums[start]);
             res.add(list);
             return res;
@@ -14,11 +14,11 @@ public class Solution {
         List<List<Integer>> lists = permute(nums, start+1);
         for(List<Integer> list : lists) {
             for(int i=0;i<list.size();i++) {
-                List<Integer> tmp = new ArrayList<Integer>(list);
+                List<Integer> tmp = new LinkedList<Integer>(list);
                 tmp.add(i, nums[start]);
                 res.add(tmp);
             }
-            List<Integer> tmp = new ArrayList<Integer>(list);
+            List<Integer> tmp = new LinkedList<Integer>(list);
             tmp.add(nums[start]);
             res.add(tmp);
         }
