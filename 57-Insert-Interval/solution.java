345416957e52790e78717c585c4e1d89f454f4a8
stonebai <dairystone@gmail.com>
2016-04-04 00:04:31 -0400

    [57. Insert Interval][Accepted]committed by LeetCode Extension

--- 57-Insert-Interval/solution.java
+++ 57-Insert-Interval/solution.java
@@ -23,24 +23,16 @@ public class Solution {
         for(int i=0;i<intervals.size();i++) {
             Interval tmp = intervals.get(i);
             if(tmp.end<newInterval.start) continue;
-            if(tmp.start>newInterval.end) break;
+            if(tmp.start>newInterval.end) {
+                intervals.add(i, newInterval);
+                return intervals;
+            }
             intervals.remove(i);
             i--;
             newInterval.start = Math.min(newInterval.start, tmp.start);
             newInterval.end = Math.max(newInterval.end, tmp.end);
         }
         intervals.add(newInterval);
-        Collections.sort(intervals, new Comparator<Interval>() {
-            @Override
-            public int compare(Interval a, Interval b) {
-                if(a.start!=b.start) {
-                    return a.start - b.start;
-                }
-                else {
-                    return a.end - b.end;
-                }
-            }
-        });
         return intervals;
     }
 }
