3f855ace24d5a3f0c0bc2ec93f1ed3b251335bf4
stonebai <dairystone@gmail.com>
2016-04-04 00:02:56 -0400

    [57. Insert Interval][Accepted]committed by LeetCode Extension

---
+++ 57-Insert-Interval/solution.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        Collections.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval a, Interval b) {
+                if(a.start!=b.start) {
+                    return a.start - b.start;
+                }
+                else {
+                    return a.end - b.end;
+                }
+            }
+        });
+        for(int i=0;i<intervals.size();i++) {
+            Interval tmp = intervals.get(i);
+            if(tmp.end<newInterval.start) continue;
+            if(tmp.start>newInterval.end) break;
+            intervals.remove(i);
+            i--;
+            newInterval.start = Math.min(newInterval.start, tmp.start);
+            newInterval.end = Math.max(newInterval.end, tmp.end);
+        }
+        intervals.add(newInterval);
+        Collections.sort(intervals, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval a, Interval b) {
+                if(a.start!=b.start) {
+                    return a.start - b.start;
+                }
+                else {
+                    return a.end - b.end;
+                }
+            }
+        });
+        return intervals;
+    }
+}
