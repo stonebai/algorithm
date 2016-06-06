eec88c321384bf3e4bced6d3295bb6dcec99b7d2
stonebai <dairystone@gmail.com>
2016-06-05 22:42:30 -0700

    [44. Wildcard Matching][Runtime Error]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,35 +1,34 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
-            return false;
-        }
-        if (p.length() == 0) {
-            if (s.length() == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        if (s.length() == 0) {
-            if (p.length() > 0 && p.charAt(0) == '*') {
-                return isMatch(s, p.substring(1));
-            } else {
-                return false;
-            }
-        }
+        if (s == null || p == null) return false;
         int i = 0, j = 0;
-        while (i < s.length() && j < p.length()) {
+        int tmpI = -1, tmpJ = -1;
+        while(i < s.length()) {
             char a = s.charAt(i), b = p.charAt(j);
             if (b == '*') {
-                return isMatch(s.substring(i + 1), p.substring(j)) || 
-                isMatch(s.substring(i), p.substring(j + 1));
-            } else if (b == '.' || a == b) {
+                j++;
+                while(j < p.length() && p.charAt(j) == '*') {
+                    j++;
+                }
+                tmpI = i;
+                tmpJ = j;
+            } else if (b == '?' && b == a) {
                 i++;
+                j++;
+            } else if (tmpJ != -1) {
+                i = ++tmpI;
+                j = tmpJ;
+            } else {
+                return false;
+            }
+        }
+        while(j < p.length()) {
+            if (p.charAt(j) == '*') {
                 j++;
             } else {
                 return false;
             }
         }
-        return i == s.length() && j == p.length();
+        return true;
     }
 }
