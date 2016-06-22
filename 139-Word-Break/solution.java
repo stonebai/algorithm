f94dbcf3d826ebe5161e680d52bb5d10cdf22dd2
stonebai <dairystone@gmail.com>
2016-06-21 23:59:45 -0700

    [139. Word Break][Compile Error]committed by LeetCode Extension

---
+++ 139-Word-Break/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    
+    Map<String, Boolean> visited = new HashMap<String, Boolean>();
+    
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if (s.equals("") || wordDict.contains(s)) {
+            return true;
+        }
+        if (visited.containsKey(s)) {
+            return visited.get(s);
+        }
+        
+        for (int i = 1;i < s.length();i++) {
+            String str = s.substring(0, i);
+            if (wordDict.contains(str) && wordBreak(s.substring(i)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
