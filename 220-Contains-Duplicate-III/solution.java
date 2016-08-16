dfbe577af2e235c7a14b44a52783cbe2833b7f6a
stonebai <dairystone@gmail.com>
2016-08-16 00:02:01 -0700

    [220. Contains Duplicate III][Time Limit Exceeded]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         for (int i = 0;i < nums.length;i++) {
-            for (int j = i + 1;j <= i + k;j++) {
-                if (Math.abs(nums[i]-nums[j]) <= t) {
+            for (int j = i + 1;j <= i + k && j < nums.length;j++) {
+                if (Math.abs((long)nums[i]-(long)nums[j]) <= t) {
                     return true;
                 }
             }
