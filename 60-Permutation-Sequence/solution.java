e2f2889445e083c98abe29bef69887c5027e7b53
stonebai <dairystone@gmail.com>
2016-08-20 00:52:51 +0800

    [60. Permutation Sequence][Time Limit Exceeded]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,16 +1,26 @@
 public class Solution {
     public String getPermutation(int n, int k) {
-        int[] nums = generateNums(n);
-        for (int i = 1;i < k;i++) {
+        int m = 1, j = 1;
+        while (m <= k) {
+            j++;
+            m *= j;
+        }
+        m = m / j - 1;
+        int[] nums = generateNums(n, j - 2);
+        for (int i = 1;i < k - m;i++) {
             nextPermutation(nums);
         }
         return toString(nums);
     }
     
-    private int[] generateNums(int n) {
+    private int[] generateNums(int n, int m) {
         int[] res = new int[n];
         for (int i = 1;i <= n;i++) {
-            res[i - 1] = i;
+            if (i < n - m) {
+                res[i - 1] = i;
+            } else {
+                res[i - 1] = 2 * n - m - i;
+            }
         }
         return res;
     }
