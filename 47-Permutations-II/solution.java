fa667ecb91dd91cb357fee9fc5aeb06ff9bcd5c5
stonebai <dairystone@gmail.com>
2016-08-19 20:19:39 +0800

    [47. Permutations II][Accepted]committed by LeetCode Extension

--- 47-Permutations-II/solution.java
+++ 47-Permutations-II/solution.java
@@ -1,31 +1,32 @@
 public class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> empty = new ArrayList<Integer>();
-        res.add(empty);
+        if (nums == null || nums.length == 0) return res;
+        res.add(new ArrayList<Integer>());
         for (int num : nums) {
-            res = permute(res, num);
-        }
-        return res;
-    }
-    
-    private List<List<Integer>> permute(List<List<Integer>> lists, int num) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        for (List<Integer> list : lists) {
-            for (int i = 0;i < list.size();i++) {
-                if (list.get(i) == num) {
-                    continue;
+            Set<List<Integer>> lists = new HashSet<List<Integer>>();
+            for (List<Integer> list : res) {
+                for (int i = 0;i < list.size();i++) {
+                    if (list.get(i) != num) {
+                        list.add(i, num);
+                        lists.add(new ArrayList<Integer>(list));
+                        list.remove(i);
+                    }
                 }
-                List<Integer> tmp = new ArrayList<Integer>(list);
-                tmp.add(i, num);
-                res.add(tmp);
+                list.add(num);
+                lists.add(list);
             }
-            if (list.get(list.size() - 1) == num) {
-                List<Integer> tmp = new ArrayList<Integer>(list);
-                tmp.add(num);
-                res.add(tmp);
-            }
+            res = new ArrayList<List<Integer>>(lists);
         }
+        // Collections.sort(res, new Comparator<List<Integer>>() {
+        //     @Override
+        //     public int compare(List<Integer> a, List<Integer> b) {
+        //         int i = 0;
+        //         while (i < a.size() && a.get(i) == b.get(i)) i++;
+        //         if (i == a.size()) return 0;
+        //         else return a.get(i) - b.get(i);
+        //     }
+        // });
         return res;
     }
 }
