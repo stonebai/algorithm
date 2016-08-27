335b0885d17f9e608f37fcad43c71805449c9432
stonebai <dairystone@gmail.com>
2016-08-28 05:40:16 +0800

    [17. Letter Combinations of a Phone Number][Accepted]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -2,15 +2,18 @@ public class Solution {
     
     private static Map<Character, List<Character>> map = new HashMap<Character, List<Character>>();
     
+    static {
+        map.put('2', Arrays.asList('a', 'b', 'c'));
+        map.put('3', Arrays.asList('d', 'e', 'f'));
+        map.put('4', Arrays.asList('g', 'h', 'i'));
+        map.put('5', Arrays.asList('j', 'k', 'l'));
+        map.put('6', Arrays.asList('m', 'n', 'o'));
+        map.put('7', Arrays.asList('p', 'q', 'r', 's'));
+        map.put('8', Arrays.asList('t', 'u', 'v'));
+        map.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+    }
+    
     public List<String> letterCombinations(String digits) {
-        map.put('2', Arrays.asList({'a', 'b', 'c'}));
-        map.put('3', Arrays.asList({'d', 'e', 'f'}));
-        map.put('4', Arrays.asList({'g', 'h', 'i'}));
-        map.put('5', Arrays.asList({'j', 'k', 'l'}));
-        map.put('6', Arrays.asList({'m', 'n', 'o'}));
-        map.put('7', Arrays.asList({'p', 'q', 'r', 's'}));
-        map.put('8', Arrays.asList({'t', 'u', 'v'}));
-        map.put('9', Arrays.asList({'w', 'x', 'y', 'z'}));
         List<String> res = new ArrayList<String>();
         res.add("");
         char[] chs = digits.toCharArray();
@@ -25,6 +28,9 @@ public class Solution {
                 }
                 res = tmp;
             }
+        }
+        if (res.size() == 1) {
+            res.remove(0);
         }
         return res;
     }
