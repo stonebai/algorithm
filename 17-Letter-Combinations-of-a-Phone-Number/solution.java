4e29c2e916a819649e473b7361c95d86c353e177
stonebai <dairystone@gmail.com>
2016-08-28 05:36:06 +0800

    [17. Letter Combinations of a Phone Number][Compile Error]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     
     private static Map<Character, List<Character>> map = new HashMap<Character, List<Character>>();
     
-    static {
+    public List<String> letterCombinations(String digits) {
         map.put('2', Arrays.asList({'a', 'b', 'c'}));
         map.put('3', Arrays.asList({'d', 'e', 'f'}));
         map.put('4', Arrays.asList({'g', 'h', 'i'}));
@@ -11,9 +11,6 @@ public class Solution {
         map.put('7', Arrays.asList({'p', 'q', 'r', 's'}));
         map.put('8', Arrays.asList({'t', 'u', 'v'}));
         map.put('9', Arrays.asList({'w', 'x', 'y', 'z'}));
-    }
-    
-    public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<String>();
         res.add("");
         char[] chs = digits.toCharArray();
