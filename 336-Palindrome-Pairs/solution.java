8ffc192a3ac31b6098b1dd66f6a82b4f8d17ac31
stonebai <dairystone@gmail.com>
2016-08-28 04:55:15 +0800

    [336. Palindrome Pairs][Accepted]committed by LeetCode Extension

---
+++ 336-Palindrome-Pairs/solution.java
@@ -0,0 +1,50 @@
+public class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        Set<List<Integer>> res = new HashSet<List<Integer>>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0;i < words.length;i++) {
+            map.put(words[i], i);
+        }
+        for (int i = 0;i < words.length;i++) {
+            map.remove(words[i]);
+            for (int j = 0;j <= words[i].length();j++) {
+                String str = words[i].substring(j);
+                if (isPalindrome(str)) {
+                    String tmp = new StringBuilder(words[i].substring(0, j)).reverse().toString();
+                    if (map.containsKey(tmp)) {
+                        save(res, i, map.get(tmp));
+                    }
+                }
+            }
+            for (int j = 0;j <= words[i].length();j++) {
+                String str = words[i].substring(0, j);
+                if (isPalindrome(str)) {
+                    String tmp = new StringBuilder(words[i].substring(j)).reverse().toString();
+                    if (map.containsKey(tmp)) {
+                        save(res, map.get(tmp), i);
+                    }
+                }
+            }
+            map.put(words[i], i);
+        }
+        return new ArrayList<List<Integer>>(res);
+    }
+    
+    private boolean isPalindrome(String str) {
+        char[] chs = str.toCharArray();
+        int i = 0, j = chs.length - 1;
+        while (i < j) {
+            if (chs[i++] != chs[j--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void save(Set<List<Integer>> res, int a, int b) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(a);
+        list.add(b);
+        res.add(list);
+    }
+}
