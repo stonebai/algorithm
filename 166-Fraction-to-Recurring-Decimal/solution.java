bf1e9b540b2479166854096e84e8d100ebf5ee1c
stonebai <dairystone@gmail.com>
2016-08-29 04:06:38 +0800

    [166. Fraction to Recurring Decimal][Accepted]committed by LeetCode Extension

--- 166-Fraction-to-Recurring-Decimal/solution.java
+++ 166-Fraction-to-Recurring-Decimal/solution.java
@@ -1,7 +1,9 @@
 public class Solution {
-    public String fractionToDecimal(int a, int b) {
-        if (a == 0) return "0";
+    public String fractionToDecimal(int x, int y) {
+        if (x == 0) return "0";
         StringBuilder sb = new StringBuilder();
+        long a = x;
+        long b = y;
         if (a > 0) {
             if (b < 0) {
                 b = -b;
@@ -21,21 +23,32 @@ public class Solution {
             return sb.toString();
         }
         sb.append('.');
-        Set<Integer> set = new HashSet<Integer>();
-        set.add(a);
-        String str = "";
+        Map<Long, Integer> map = new HashMap<Long, Integer>();
+        map.put(a, 0);
+        List<Long> list = new ArrayList<Long>();
         while (a != 0) {
             a *= 10;
-            str = str + a / b;
+            list.add(a / b);
             a %= b;
-            if (set.contains(a)) {
+            if (map.containsKey(a)) {
+                int index = map.get(a);
+                int i = 0;
+                while (i < index) {
+                    sb.append(list.get(i++));
+                }
                 sb.append('(');
-                sb.append(str);
+                while (i < list.size()) {
+                    sb.append(list.get(i++));
+                }
                 sb.append(')');
                 return sb.toString();
+            } else {
+                map.put(a, list.size());
             }
         }
-        sb.append(str);
+        for (int i = 0;i < list.size();i++) {
+            sb.append(list.get(i));
+        }
         return sb.toString();
     }
 }
