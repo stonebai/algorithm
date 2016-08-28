71760e790687b024f90631fa6f70ff70def4c787
stonebai <dairystone@gmail.com>
2016-08-29 05:45:59 +0800

    [97. Interleaving String][Time Limit Exceeded]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -1,53 +1,34 @@
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1 == null || s2 == null || s3 == null || s1.length() + s2.length() != s3.length()) {
-            return false;
-        }
-        if (s1.length() == 0) {
-            return s2.equals(s3);
-        }
-        if (s2.length() == 0) {
-            return s1.equals(s3);
-        }
+        if (s3.length() != s1.length() + s2.length()) return false;
         char[] chs1 = s1.toCharArray();
         char[] chs2 = s2.toCharArray();
         char[] chs3 = s3.toCharArray();
-        return helper(chs1, chs2, chs3, 0, 0, 0, new short[chs1.length + 1][chs2.length + 1][chs3.length + 1]);
-    }
-    
-    private boolean helper(char[] chs1, char[] chs2, char[] chs3, int i, int j, int k, short[][][] dp) {
-        if (dp[i][j][k] != 0) {
-            if (dp[i][j][k] == 1) return true;
-            else return false;
-        }
-        int ii = i, jj = j, kk = k;
-        while (k < chs3.length && i < chs1.length && j < chs2.length) {
-            if (chs3[k] == chs1[i] && chs3[k] == chs2[j]) {
-                if (helper(chs1, chs2, chs3, i + 1, j, k + 1, dp)) return true;
-                else return helper(chs1, chs2, chs3, i, j + 1, k + 1, dp);
-            } else if (chs3[k] == chs1[i]) {
+        int i = 0, j = 0, k = 0;
+        while (i < chs1.length && j < chs2.length && k < chs3.length) {
+            if (chs1[i] == chs3[k] && chs2[j] != chs3[k]) {
                 i++;
-            } else if (chs3[k] == chs2[j]) {
+                k++;
+            } else if (chs1[i] != chs3[k] && chs2[j] == chs3[k]) {
                 j++;
+                k++;
+            } else if (chs1[i] == chs3[k] && chs2[j] == chs3[k]) {
+                return isInterleave(s1.substring(i + 1), s2.substring(j), s3.substring(k + 1))
+                || isInterleave(s1.substring(i), s2.substring(j + 1), s3.substring(k + 1));
             } else {
-                dp[ii][jj][kk] = -1;
-                return false;
-            }
-            k++;
-        }
-        while (k < chs3.length && i < chs1.length) {
-            if (chs3[k++] != chs1[i++]) {
-                dp[ii][jj][kk] = -1;
                 return false;
             }
         }
-        while (k < chs3.length && j < chs2.length) {
-            if (chs3[k++] != chs2[j++]) {
-                dp[ii][jj][kk] = -1;
+        while (i < chs1.length && k < chs3.length) {
+            if (chs1[i++] != chs3[k++]) {
                 return false;
             }
         }
-        dp[ii][jj][kk] = 1;
+        while (j < chs2.length && k < chs3.length) {
+            if (chs2[j++] != chs3[k++]) {
+                return false;
+            }
+        }
         return true;
     }
 }
