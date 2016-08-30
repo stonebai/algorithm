6fdadb8aa85f282eab12a0e5465449e60d002cca
stonebai <dairystone@gmail.com>
2016-08-30 11:36:01 -0400

    [327. Count of Range Sum][Time Limit Exceeded]committed by LeetCode Extension

--- 327-Count-of-Range-Sum/solution.java
+++ 327-Count-of-Range-Sum/solution.java
@@ -2,15 +2,23 @@ public class Solution {
     public int countRangeSum(int[] nums, int lower, int upper) {
         int res = 0;
         long prefixSum = 0;
-        TreeSet<Long> set = new TreeSet<Long>();
-        set.add(0);
+        TreeMap<Long, Integer> map = new TreeMap<Long, Integer>();
+        map.put((long) 0, 1);
         for (int num : nums) {
             prefixSum += num;
             // cur - pre >= lower => pre <= cur - lower
             // cur - pre <= upper => pre >= cur - upper
-            Set<Long> tmp = set.subSet(prefixSum - upper, true, prefixSum - lower, true);
-            res += tmp.size();
-            set.add(prefixSum);
+            Map<Long, Integer> m = map.subMap(prefixSum - upper, true, prefixSum - lower, true);
+            if (m != null) {
+                for (Map.Entry<Long, Integer> entry : m.entrySet()) {
+                    res += entry.getValue();
+                }
+            }
+            if (map.containsKey(prefixSum)) {
+                map.put(prefixSum, map.get(prefixSum) + 1);
+            } else {
+                map.put(prefixSum, 1);
+            }
         }
         return res;
     }
