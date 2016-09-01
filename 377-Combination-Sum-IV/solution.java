5d48b452097b02129d722585862f4b0f59323d64
stonebai <dairystone@gmail.com>
2016-09-01 15:21:54 -0400

    [377. Combination Sum IV][Accepted]committed by LeetCode Extension

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -1,18 +1,25 @@
 public class Solution {
     public int combinationSum4(int[] nums, int target) {
         if (target <= 0) return 0;
+        Arrays.sort(nums);
         return dfs(nums, target, new HashMap<Integer, Integer>());
     }
     
     private int dfs(int[] nums, int target, Map<Integer, Integer> dp) {
-        if (target < 0) return 0;
-        if (target == 0) return 1;
         if (dp.containsKey(target)) {
             return dp.get(target);
         }
         int res = 0;
         for (int num : nums) {
-            res += dfs(nums, target - num, dp);
+            int delta = target - num;
+            if (delta > 0) {
+                res += dfs(nums, target - num, dp);
+            } else {
+                if (delta == 0) {
+                    res += 1;
+                }
+                break;
+            }
         }
         dp.put(target, res);
         return res;
