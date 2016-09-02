33bcb90c8a41add792b5a6e413a086baf4a9c22c
stonebai <dairystone@gmail.com>
2016-09-02 11:39:21 -0400

    [334. Increasing Triplet Subsequence][Time Limit Exceeded]committed by LeetCode Extension

--- 334-Increasing-Triplet-Subsequence/solution.java
+++ 334-Increasing-Triplet-Subsequence/solution.java
@@ -1,37 +1,32 @@
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if(nums==null || nums.length<3) return false;
-        List<Integer> list = new ArrayList<Integer>(nums.length);
+        if (nums == null || nums.length < 3) return false;
+        List<Integer> list = new ArrayList<Integer>();
         list.add(nums[0]);
-        for(int i=1;i<nums.length;i++) {
-            int max = list.get(list.size()-1);
-            if(nums[i]>max) {
-                list.add(nums[i]);
-            }
-            else if(nums[i]<max) {
-                int index = findLeastLargerOrEqual(list, nums[i]);
-                list.set(index, nums[i]);
-            }
-            if(list.size()>=3) return true;
+        for (int i = 1;i < nums.length;i++) {
+            update(list, nums[i]);
+            if (list.size() == 3) return true;
         }
         return false;
     }
     
-    private int findLeastLargerOrEqual(List<Integer> list, int target) {
-        int start = 0, end = list.size() - 1;
-        while(start<end) {
-            int mid = start + (end - start) / 2;
-            int num = list.get(mid);
-            if(num>target) {
+    private void update(List<Integer> list, int num) {
+        int end = list.size() - 1;
+        if (num > list.get(end)) {
+            list.add(num);
+            return;
+        }
+        int start = 0;
+        while (start < end) {
+            int mid = start + (end - start);
+            if (list.get(mid) > num) {
                 end = mid;
-            }
-            else if(num<target) {
+            } else if (list.get(mid) < num) {
                 start = mid + 1;
-            }
-            else {
-                return mid;
+            } else {
+                return;
             }
         }
-        return end;
+        list.set(start, num);
     }
 }
