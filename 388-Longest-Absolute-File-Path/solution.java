eaf19335354d0a36aa0aac4912e61453b373fa83
stonebai <dairystone@gmail.com>
2016-09-01 20:07:34 -0400

    [388. Longest Absolute File Path][Compile Error]committed by LeetCode Extension

---
+++ 388-Longest-Absolute-File-Path/solution.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public int lengthLongestPath(String input) {
+        String[] split = input.split("\n");
+        int i = 0, res = 0;
+        while (i < split.length) {
+            if (split[i].contains(".")) {
+                res = Math.max(res, split[i].length());
+                i++;
+            } else {
+                int[] nums = dfs(split, i, 1);
+                res = Math.max(res, nums[0]);
+                i = nums[1];
+            }
+        }
+        return res;
+    }
+    
+    private int[] dfs(String[] split, int index, int level) {
+        int[] res = new int[2];
+        String prefix = prefix(level);
+        int i = index + 1;
+        while (i < split.length && split[i].startsWith(prefix)) {
+            if (split[i].contains(".")) {
+                res[0] = Math.max(res[0], split[i].length() - prefix.length() + 1);
+                i++;
+            } else {
+                int[] nums = dfs(split, i, level + 1);
+                res[0] = Math.max(res[0], nums[0] + 1);
+                i = res[1];
+            }
+        }
+        if (res > 0) {
+            return res - prefix.length() + split[index].length() + 1;
+        } else {
+            return 0;
+        }
+    }
+    
+    private String prefix(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0;i < n;i++) {
+            sb.append("\t");
+        }
+        return sb.toString();
+    }
+}
