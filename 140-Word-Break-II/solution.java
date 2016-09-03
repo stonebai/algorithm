dc4387a3dc0c81edbc78db28d33b93b4bb915fca
stonebai <dairystone@gmail.com>
2016-09-02 22:56:51 -0400

    [140. Word Break II][Compile Error]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,29 +1,61 @@
 public class Solution {
     
-    private Map<String, List<String>> map = new HashMap<String, List<String>>();
+    class Node {
+        boolean isWord;
+        Node[] next;
+        
+        Node() {
+            this.isWord = false;
+            this.next = new Node[26];
+        }
+    }
     
     public List<String> wordBreak(String s, Set<String> wordDict) {
-        if(map.containsKey(s)) return map.get(s);
-        List<String> res = new ArrayList<String>();
-        for(int j = s.length() - 1; j >= 0; j--){
-            if(wordDict.contains(s.substring(j)))
-                break;
-            else{
-                if(j == 0)
-                    return res;
-            }
+        Node root = buildTrie(Set<String> wordDict);
+        return dfs(s, root, new HashMap<String, List<String>>());
+    }
+    
+    private List<String> dfs(String s, Node root, Map<String, List<String>> map) {
+        if (map.containsKey(s)) {
+            return map.get(s);
         }
-        for(int i=1;i<=s.length();i++) {
-            String t = s.substring(0, i);
-            if(wordDict.contains(t)) {
-                List<String> list = wordBreak(s.substring(i), wordDict);
-                for(String str : list) {
-                    res.add(t + " " + str);
+        List<String> res = new ArrayList<String>();
+        char[] chs = s.toCharArray();
+        Node cur = root;
+        for (int i = 0;i < chs.length;i++) {
+            int offset = chs[i] - 'a';
+            if (cur.next[offset] == null) {
+                map.put(s, res);
+                return res;
+            } else {
+                cur = cur.next[offset];
+                if (cur.isWord) {
+                    List<String> list = dfs(s.substring(i + 1), root, map);
+                    String prefix = s.substring(0, i + 1);
+                    for (String str : list) {
+                        res.add(prefix + " " + str);
+                    }
                 }
             }
         }
-        if(wordDict.contains(s)) res.add(s);
         map.put(s, res);
         return res;
+    }
+    
+    private Node buildTrie(Set<String> wordDict) {
+        Node root = new Node();
+        for (String word : wordDict) {
+            Node cur = root;
+            char[] chs = word.toCharArray();
+            for (char ch : chs) {
+                int offset = ch - 'a';
+                if (cur.next[offset] == null) {
+                    cur.next[offset] = new Node();
+                }
+                cur = cur.next[offset];
+            }
+            cur.isWord = true;
+        }
+        return root;
     }
 }
