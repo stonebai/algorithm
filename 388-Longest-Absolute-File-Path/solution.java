3823b111f56ab4949aa75ddfc8f7f974af20eca8
stonebai <dairystone@gmail.com>
2016-09-04 15:41:57 -0400

    [388. Longest Absolute File Path][Accepted]committed by LeetCode Extension

--- 388-Longest-Absolute-File-Path/solution.java
+++ 388-Longest-Absolute-File-Path/solution.java
@@ -1,6 +1,10 @@
 public class Solution {
+    
+    private static final String LINE_SPLITER = "\n";
+    private static final char INDENT = '\t';
+    
     public int lengthLongestPath(String input) {
-        String[] split = input.split("\n");
+        String[] split = input.split(LINE_SPLITER);
         int i = 0, res = 0;
         while (i < split.length) {
             if (split[i].contains(".")) {
@@ -17,11 +21,10 @@ public class Solution {
     
     private int[] dfs(String[] split, int index, int level) {
         int[] res = new int[2];
-        String prefix = prefix(level);
         int i = index + 1;
-        while (i < split.length && split[i].startsWith(prefix)) {
+        while (i < split.length && validatePrefix(split[i], level)) {
             if (split[i].contains(".")) {
-                res[0] = Math.max(res[0], split[i].length() - prefix.length());
+                res[0] = Math.max(res[0], split[i].length() - level);
                 i++;
             } else {
                 int[] nums = dfs(split, i, level + 1);
@@ -30,17 +33,18 @@ public class Solution {
             }
         }
         if (res[0] > 0) {
-            res[0] += split[index].length() - prefix.length() + 2;
+            res[0] += split[index].length() - level + 2;
         }
         res[1] = i;
         return res;
     }
     
-    private String prefix(int n) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0;i < n;i++) {
-            sb.append("\t");
+    private boolean validatePrefix(String str, int num) {
+        for (int i = 0;i < num;i++) {
+            if (str.charAt(i) != INDENT) {
+                return false;
+            }
         }
-        return sb.toString();
+        return true;
     }
 }
