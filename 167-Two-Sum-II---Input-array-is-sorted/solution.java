e76076615d1d2675c69d25d2f0017d08b2f0e2a0
stonebai <dairystone@gmail.com>
2016-09-04 23:57:46 -0400

    [167. Two Sum II - Input array is sorted][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            long tmp = nums[i] + nums[j];
+            if (tmp < target) {
+                i++;
+            } else if (tmp > target) {
+                j--;
+            } else {
+                res[0] = i;
+                res[1] = j;
+            }
+        }
+        return res;
+    }
+}
