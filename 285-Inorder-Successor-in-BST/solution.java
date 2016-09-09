ce388d179fdbf4ff84f42fa19a50727500bb69df
stonebai <dairystone@gmail.com>
2016-09-09 14:13:09 -0400

    [285. Inorder Successor in BST][Accepted]committed by LeetCode Extension

--- 285-Inorder-Successor-in-BST/solution.java
+++ 285-Inorder-Successor-in-BST/solution.java
@@ -9,38 +9,39 @@
  */
 public class Solution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        if (root == null || p == null) {
-            return null;
-        } else {
-            return dfs(root, p);
-        }
-    }
-    
-    private TreeNode dfs(TreeNode root, TreeNode p) {
-        if (root == null) {
-            return null;
-        }
-        if (root.val == p.val) {
-            return mostLeft(root.right);
-        } else if (root.val < p.val) {
-            return dfs(root.right, p);
-        } else {
-            TreeNode tmp = dfs(root.left, p);
-            if (tmp != null) {
-                return tmp;
+        Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
+        deque.offerFirst(root);
+        TreeNode pre = null;
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.pollFirst();
+            if (node.val == p.val) {
+                if (node.right != null) {
+                    return mostLeft(node.right);
+                } else {
+                    return pre;
+                }
+            } else if (node.val > p.val) {
+                if (node.left != null) {
+                    deque.offerFirst(node.left);
+                    pre = node;
+                } else {
+                    return null;
+                }
             } else {
-                return root;
+                if (node.right != null) {
+                    deque.offerFirst(node.right);
+                } else {
+                    return null;
+                }
             }
         }
+        return null;
     }
     
-    private TreeNode mostLeft(TreeNode root) {
-        if (root == null) {
-            return null;
-        } else if (root.left != null) {
-            return mostLeft(root.left);
-        } else {
-            return root;
+    private TreeNode mostLeft(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
         }
+        return node;
     }
 }
