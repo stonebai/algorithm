cf2cad5c030f51b43454524da71966df369ce373
stonebai <dairystone@gmail.com>
2016-09-08 23:38:13 -0400

    [311. Sparse Matrix Multiplication][Runtime Error]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -17,8 +17,8 @@ public class Solution {
         }
         for (int i = 0;i < row;i++) {
             for (int j = 0;j < col;j++) {
-                for (int index : ma[i]) {
-                    res[i][j] += A[i][index] * B[index][j];
+                for (Object index : ma[i]) {
+                    res[i][j] += A[i][(int) index] * B[(int) index][j];
                 }
             }
         }
