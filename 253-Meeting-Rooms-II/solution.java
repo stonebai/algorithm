9b650fd563970b9764012e266c7cfa34054076dc
stonebai <dairystone@gmail.com>
2016-09-09 10:57:07 -0400

    [253. Meeting Rooms II][Compile Error]committed by LeetCode Extension

---
+++ 253-Meeting-Rooms-II/solution.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    
+    private static class Node implements Comparaber<Node> {
+        int x;
+        boolean isStart;
+        
+        Node(int x, boolean isStart) {
+            this.x = x;
+            this.isStart = isStart;
+        }
+        
+        @Override
+        public int compareTo(Node o) {
+            if (this.x != o.x) {
+                return this.x - o.x;
+            } else {
+                if (this.isStart && o.isStart) {
+                    return 0;
+                } else if (this.isStart) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        }
+    }
+    
+    public int minMeetingRooms(Interval[] intervals) {
+        Queue<Node> q = preprocess(intervals);
+        int count = 0, res = 0;
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            if (node.isStart) {
+                count++;
+            } else {
+                count--;
+            }
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+    
+    private Queue<Node> preprocess(Interval[] intervals) {
+        Queue<Node> q = new PriorityQueue<Node>();
+        for (Interval interval : intervals) {
+            q.offer(new Node(interval.start, true));
+            q.offer(new Node(interval.end, false));
+        }
+        return q;
+    }
+}
