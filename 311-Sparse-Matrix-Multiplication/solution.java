c9c32ab099e686f2be6bb0c304862f992aad999b
stonebai <dairystone@gmail.com>
2016-09-08 23:37:48 -0400

    [311. Sparse Matrix Multiplication][Compile Error]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -1,17 +1,25 @@
 public class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
-        if (A.length == 0 || B.length == 0) {
-            return new int[0][0];
-        }
-        int row = A.length, col = B[0].length, len = B.length;
+        int row = A.length, col = B.length;
         int[][] res = new int[row][col];
+        if (row == 0 || col == 0) {
+            return res;
+        }
+        int n = A[0].length;
+        List[] ma = new List[row];
+        for (int i = 0;i < row;i++) {
+            ma[i] = new ArrayList<Integer>();
+            for (int j = 0;j < n;j++) {
+                if (A[i][j] != 0) {
+                    ma[i].add(j);
+                }
+            }
+        }
         for (int i = 0;i < row;i++) {
             for (int j = 0;j < col;j++) {
-                int sum = 0;
-                for (int k = 0;k < len;k++) {
-                    sum += A[i][k] * B[k][j];
+                for (int index : ma[i]) {
+                    res[i][j] += A[i][index] * B[index][j];
                 }
-                res[i][j] = sum;
             }
         }
         return res;
