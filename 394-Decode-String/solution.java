065bc62a130b01f9da2414da87191b824c78cebf
stonebai <dairystone@gmail.com>
2016-09-10 16:02:07 -0400

    [394. Decode String][Compile Error]committed by LeetCode Extension

--- 394-Decode-String/solution.java
+++ 394-Decode-String/solution.java
@@ -1,55 +1,29 @@
 public class Solution {
-    
-    static class Node {
-        String str;
-        int index;
-        
-        Node(String str, int index) {
-            this.str = str;
-            this.index = index;
-        }
-    }
-    
     public String decodeString(String s) {
+        Stack<StringBuilder> stack = new Stack<StringBuilder>();
+        Stack<Integer> s = new Stack<Integer>();
         StringBuilder sb = new StringBuilder();
         char[] chs = s.toCharArray();
         int num = 0;
         for (int i = 0;i < chs.length;i++) {
             if (chs[i] >= '0' && chs[i] <= '9') {
                 num = num * 10 - '0' + chs[i];
-            } else if (chs[i] == ']') {
-                break;
             } else if (chs[i] == '[') {
-                Node node = dfs(chs, i + 1);
-                sb.append(repeat(node.str, num));
+                stack.push(sb);
+                s.push(num);
+                sb = new StringBuilder();
                 num = 0;
-                i = node.index;
+            } else if (chs[i] == ']') {
+                int num = s.pop();
+                String str = repeat(sb.toString(), num);
+                sb = stack.pop();
+                sb.append(str);
+                num = 0;
             } else {
                 sb.append(chs[i]);
             }
         }
         return sb.toString();
-    }
-    
-    private Node dfs(char[] chs, int i) {
-        StringBuilder sb = new StringBuilder();
-        int num = 0;
-        while (i < chs.length) {
-            if (chs[i] >= '0' && chs[i] <= '9') {
-                num = num * 10 - '0' + chs[i];
-            } else if (chs[i] == ']') {
-                return new Node(sb.toString(), i);
-            } else if (chs[i] == '[') {
-                Node node = dfs(chs, i + 1);
-                sb.append(repeat(node.str, num));
-                num = 0;
-                i = node.index;
-            } else {
-                sb.append(chs[i]);
-            }
-            i++;
-        }
-        return new Node(sb.toString(), i + 1);
     }
     
     private String repeat(String str, int k) {
