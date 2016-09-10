bb8cdd31e0d3ddc34eddd34d768c043affa1ad51
stonebai <dairystone@gmail.com>
2016-09-10 14:36:29 -0400

    [23. Merge k Sorted Lists][Runtime Error]committed by LeetCode Extension

---
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        Queue<ListNode> q = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode a, ListNode b) {
+                return a.val - b.val;
+            }
+        });
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (!q.isEmpty()) {
+            ListNode node = q.poll();
+            cur.next = node;
+            cur = node;
+            if (node.next != null) {
+                q.offer(node.next);
+            }
+        }
+        return dummy.next;
+    }
+}
