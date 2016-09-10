3bb9a6ae06460218af743a8f8ab08898994fc0de
stonebai <dairystone@gmail.com>
2016-09-09 22:07:55 -0400

    [10. Regular Expression Matching][Compile Error]committed by LeetCode Extension

---
+++ 10-Regular-Expression-Matching/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        char[] chs = s.toCharArray();
+        char[] chp = p.toCharArray();
+        boolean[][] dp = new boolean[chs.length + 1][chp.length + 1];
+        int i = 0, j = 0;
+        return dfs(chs, chp, i, j);
+    }
+    
+    private boolean dfs(char[] chs, char[] chp, int i, int j) {
+        while (i < chs.length && j < chp.length) {
+            if (j < chp.length - 1 && chp[j + 1] == '*') {
+                if (chp[j] == '.') {
+                    return true;
+                }
+                if (chs[i] != chp[j]) {
+                    return dfs(chs, chp, i, j + 2);
+                }
+                return dfs(chs, chp, i, j + 2) || dfs(chs, chp, i + 1, j);
+            }
+            if (chp[j] == '.' || chs[i] == chp[j]) {
+                i++;
+                j++;
+            }
+        }
+        if (i != chs.length) {
+            return false;
+        }
+        if (j != chp.length) {
+            if (j = chp.length - 2 && chp[j + 1] == '*') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
