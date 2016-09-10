e0e2b20bc60c6ca5be9f71bca6498166499b318b
stonebai <dairystone@gmail.com>
2016-09-10 16:25:37 -0400

    [323. Number of Connected Components in an Undirected Graph][Compile Error]committed by LeetCode Extension

---
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] uf = new int[n];
+        for (int i = 0;i < n;i++) {
+            uf[i] = i;
+        }
+        int count = n;
+        for (int[] edge : edges) {
+            if (!isConnected(uf, edge[0], edge[1])) {
+                union(uf, edge[0], edge[1]);
+                count--;
+            }
+        }
+    }
+    
+    private void union(int[] uf, int i, int j) {
+        int rootI = root(uf, i);
+        int rootJ = root(uf, j);
+        uf[rootI] = rootJ;
+    }
+    
+    private int root(int[] uf, int i) {
+        while (i != uf[i]) {
+            i = uf[i];
+        }
+        return i;
+    }
+    
+    private boolean isConnected(int[] uf, int i, int j) {
+        int rootI = root(uf, i);
+        int rootJ = root(uf, j);
+        return rootI == rootJ;
+    }
+}
