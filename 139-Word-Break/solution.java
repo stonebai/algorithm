786a90c9c2604c884b3fde93b449ab06ccf32450
stonebai <dairystone@gmail.com>
2016-09-10 15:36:58 -0400

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,11 +1,17 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
         Queue<String> q = new LinkedList<String>();
+        Set<String> visited = new HashSet<String>();
         q.offer(s);
         while (!q.isEmpty()) {
             String str = q.poll();
             if (wordDict.contains(str)) {
                 return true;
+            }
+            if (visited.contains(str)) {
+                continue;
+            } else {
+                visited.add(str);
             }
             for (int i = 1;i < str.length();i++) {
                 if (wordDict.contains(str.substring(0, i))) {
