2c67e84275210de27bf2fe14d739db525858dfd3
stonebai <dairystone@gmail.com>
2016-09-10 13:47:06 -0400

    [75. Sort Colors][Compile Error]committed by LeetCode Extension

---
+++ 75-Sort-Colors/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        int pos = 0;
+        for (int i = 0;i < nums.length;i++) {
+            if (nums[i] == 0) {
+                swap(nums, i, pos++);
+            }
+        }
+        int pos = nums.length - 1;
+        for (int i = nums.length - 1;i >= 0;i--) {
+            if (nums[i] == 2) {
+                swap(nums, i, pos--);
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
