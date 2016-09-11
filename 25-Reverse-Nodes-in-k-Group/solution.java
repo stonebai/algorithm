e8be1eaab80a9dd020bd9f08f02be091cbe16064
stonebai <dairystone@gmail.com>
2016-09-11 02:22:07 -0400

    [25. Reverse Nodes in k-Group][Accepted]committed by LeetCode Extension

--- 25-Reverse-Nodes-in-k-Group/solution.java
+++ 25-Reverse-Nodes-in-k-Group/solution.java
@@ -14,30 +14,35 @@ public class Solution {
         ListNode[] list = reverseK(head, k);
         ListNode res = list[0];
         ListNode tail = list[1];
+        ListNode next = list[2];
         while (tail != null) {
-            list = reverseK(tail, k);
+            list = reverseK(next, k);
             tail.next = list[0];
             tail = list[1];
+            next = list[2];
         }
         return res;
     }
     
     private ListNode[] reverseK(ListNode node, int k) {
-        ListNode[] res = new ListNode[2];
+        ListNode[] res = new ListNode[3];
         if (!lenK(node, k)) {
             res[0] = node;
             return res;
         }
         ListNode dummy = new ListNode(0);
+        ListNode tail = node;
+        ListNode next = null;
         while (k > 0) {
-            ListNode tmp = node.next;
+            next = node.next;
             node.next = dummy.next;
             dummy.next = node;
-            node = tmp;
+            node = next;
             k--;
         }
         res[0] = dummy.next;
-        res[1] = node;
+        res[1] = tail;
+        res[2] = next;
         return res;
     }
     
@@ -49,5 +54,6 @@ public class Solution {
             }
             node = node.next;
         }
+        return false;
     }
 }
