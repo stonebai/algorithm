fad9c27fec165655e67a7e9dbc83f4cd3988690b
stonebai <dairystone@gmail.com>
2016-09-11 02:37:55 -0400

    [85. Maximal Rectangle][Runtime Error]committed by LeetCode Extension

--- 85-Maximal-Rectangle/solution.java
+++ 85-Maximal-Rectangle/solution.java
@@ -1,47 +1,65 @@
 public class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if(matrix==null || matrix.length==0) return 0;
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int[] h = new int[col];
-        int max = 0;
-        for(int i=0;i<row;i++) {
-            for(int j=0;j<col;j++) {
-                if(matrix[i][j]=='1') h[j] += 1;
-                else h[j] = 0;
-            }
-            int count = histogram(h);
-            if(count>max) max = count;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
         }
-        return max;
+        int row = matrix.length;
+        int col = matrix.length;
+        int res = 0;
+        if (row < col) {
+            int[] dp = new int[row];
+            for (int j = 0;j < col;j++) {
+                for (int i = 0;i < row;i++) {
+                    if (matrix[i][j] == '0') {
+                        dp[i] = 0;
+                    } else {
+                        dp[i]++;
+                    }
+                }
+                res = Math.max(res, histogram(dp));
+            }
+        } else {
+            int[] dp = new int[col];
+            for (int i = 0;i < row;i++) {
+                for (int j = 0;j < col;j++) {
+                    if (matrix[i][j] == '0') {
+                        dp[j] = 0;
+                    } else {
+                        dp[j]++;
+                    }
+                }
+                res = Math.max(res, histogram(dp));
+            }
+        }
+        return res;
     }
     
-    private int histogram(int[] h) {
-        Stack<Integer> height = new Stack<Integer>();
+    private int histogram(int[] dp) {
+        Stack<Integer> h = new Stack<Integer>();
         Stack<Integer> index = new Stack<Integer>();
         int max = 0;
-        for(int i=0;i<h.length;i++) {
-            if(height.isEmpty() || h[i]>height.peek()) {
-                height.push(h[i]);
+        for (int i = 0;i < dp.length;i++) {
+            if (h.isEmpty() || h.peek() < dp[i]) {
+                h.push(dp[i]);
                 index.push(i);
-            }
-            else {
-                int preIndex = i;
-                while(!height.isEmpty() && h[i]<=height.peek()) {
-                    int preHeight = height.pop();
-                    preIndex = index.pop();
-                    int count = (i - preIndex) * preHeight;
-                    if(count>max) max = count;
+            } else {
+                int pre = index.peek();
+                while (!h.isEmpty() && h.peek() >= dp[i]) {
+                    int hi = h.pop();
+                    int in = index.pop();
+                    int area = dp[i] * (i - in + 1);
+                    max = Math.max(area, max);
+                    pre = in;
                 }
-                height.push(h[i]);
-                index.push(preIndex);
+                h.push(dp[i]);
+                index.push(pre);
             }
         }
-        while(!height.isEmpty()) {
-            int preIndex = index.pop();
-            int preHeight = height.pop();
-            int count = (h.length - preIndex) * preHeight;
-            if(count>max) max = count;
+        while (!h.isEmpty()) {
+            int hi = h.pop();
+            int in = index.pop();
+            int area = hi * (dp.length - in);
+            max = Math.max(area, max);
         }
         return max;
     }
