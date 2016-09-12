eccd273e0d44a1fb164654a2671cd5b7876a6a7a
stonebai <dairystone@gmail.com>
2016-09-12 01:19:26 -0400

    [251. Flatten 2D Vector][Compile Error]committed by LeetCode Extension

---
+++ 251-Flatten-2D-Vector/solution.java
@@ -0,0 +1,36 @@
+public class Vector2D implements Iterator<Integer> {
+    
+    private Iterator<List<Integer>> lIter;
+    private Iterator<Integer> iIter;
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        this.lIter = vec2d.iterator();
+    }
+
+    @Override
+    public Integer next() {
+        if (hasNext()) {
+            return this.iIter.next();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (this.iIter == null || !this.iIter.hasNext()) {
+            if (this.lIter.hasNext()) {
+                this.iIter = this.lIter.next();
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
