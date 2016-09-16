025181c24aa1e48d1c5c8ce04eb1d9631b592ceb
stonebai <dairystone@gmail.com>
2016-09-16 10:35:34 -0700

    [32. Longest Valid Parentheses][Compile Error]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,23 +1,34 @@
 public class Solution {
     public int longestValidParentheses(String s) {
         char[] chs = s.toCharArray();
-        Stack<Integer> stack = new Stack<Integer>();
-        int pre = 0;
-        int cur = 0;
+        Stack<Integer> ids = new Stack<Integer>();
+        Stack<Character> chs = new Stack<Integer>();
         for (int i = 0;i < chs.length;i++) {
             if (chs[i] == '(') {
-                stack.push(i);
+                ids.push(i);
+                chs.push('(');
             }
             if (chs[i] == ')') {
-                if (!stack.isEmpty()) {
-                    pre = Math.max(pre, cur);
-                    cur = 0;
+                if (!chs.isEmpty() && chs.peek() == '(') {
+                    chs.pop();
+                    ids.pop();
                 } else {
-                    int index = stack.pop();
-                    cur = i - index + 1;
+                    chs.push(')');
+                    ids.push(i);
                 }
             }
         }
-        return Math.max(pre, cur);
+        if (ids.isEmpty()) {
+            return chs.length;
+        }
+        int end = chs.length;
+        int res = 0;
+        while (!ids.isEmpty()) {
+            int start = ids.pop();
+            int len = end - start - 1;
+            res = Math.max(len, res);
+            end = start;
+        }
+        return res;
     }
 }
