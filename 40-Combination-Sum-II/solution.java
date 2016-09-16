3c6b3232feafa495b9540133f8bd4e73119aec10
stonebai <dairystone@gmail.com>
2016-09-15 20:24:30 -0700

    [40. Combination Sum II][Accepted]committed by LeetCode Extension

--- 40-Combination-Sum-II/solution.java
+++ 40-Combination-Sum-II/solution.java
@@ -1,23 +1,25 @@
 public class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        Array.sort(candidates);
+        Arrays.sort(candidates);
         helper(candidates, target, 0, 0, new ArrayList<Integer>(), res);
         return res;
     }
     
     private void helper(int[] candidates, int target, int sum, int pos, List<Integer> path, List<List<Integer>> res) {
-        if (sum > target) {
-            return;
-        } else if (sum == target) {
+        if (sum == target) {
             res.add(new ArrayList<Integer>(path));
             return;
         }
         for (int i = pos;i < candidates.length;i++) {
-            while (i < candidates.length - 1 && candidates[i] == candidates[i + 1]) i++;
+            int tmp = sum + candidates[i];
+            if (tmp > target) {
+                break;
+            }
             path.add(candidates[i]);
-            helper(candidates, target, sum + candidates[i], i + 1, path, res);
+            helper(candidates, target, tmp, i + 1, path, res);
             path.remove(path.size() - 1);
+            while (i < candidates.length - 1 && candidates[i] == candidates[i + 1]) i++;
         }
     }
 }
