2feaba9bb8ec02fb35144535ffae9235c9baa4b4
stonebai <dairystone@gmail.com>
2016-09-16 09:39:23 -0700

    [98. Validate Binary Search Tree][Runtime Error]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -8,25 +8,51 @@
  * }
  */
 public class Solution {
+    // public boolean isValidBST(TreeNode root) {
+    //     if (root == null) {
+    //         return true;
+    //     }
+    //     TreeNode pre = null;
+    //     TreeNode cur = root;
+    //     Stack<TreeNode> stack = new Stack<TreeNode>();
+    //     while (cur != null || !stack.isEmpty()) {
+    //         while (cur != null) {
+    //             stack.push(cur);
+    //             cur = cur.left;
+    //         }
+    //         cur = stack.pop();
+    //         if (pre != null && cur.val <= pre.val) {
+    //             return false;
+    //         }
+    //         pre = cur;
+    //         cur = cur.right;
+    //     }
+    //     return true;
+    // }
+    
     public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        TreeNode pre = null;
-        TreeNode cur = root;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        while (cur != null || !stack.isEmpty()) {
-            while (cur != null) {
-                stack.push(cur);
-                cur = cur.left;
+        int[] tmp = dfs(root);
+        return tmp[1] != -1;
+    }
+    
+    private int[] dfs(TreeNode root) {
+        int[] res = new int[2];
+        res[0] = root.val;
+        if (root.left != null) {
+            int[] tmp = dfs(root.left);
+            if (tmp[1] == -1 || tmp[0] >= root.val) {
+                res[1] = -1;
+                return res;
             }
-            cur = stack.pop();
-            if (pre != null && cur.val <= pre.val) {
-                return false;
-            }
-            pre = cur;
-            cur = cur.right;
         }
-        return true;
+        if (root.right != null) {
+            int[] tmp = dfs(root.right);
+            if (tmp[1] == -1 || tmp[0] <= root.val) {
+                res[1] = -1;
+                return res;
+            }
+            res[0] = tmp[0];
+        }
+        return res;
     }
 }
