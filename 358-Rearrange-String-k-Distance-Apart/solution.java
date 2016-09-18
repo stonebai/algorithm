aacce73cb1b528e87788c18497843fb943417f48
stonebai <dairystone@gmail.com>
2016-09-18 12:46:29 -0700

    [358. Rearrange String k Distance Apart][Accepted]committed by LeetCode Extension

--- 358-Rearrange-String-k-Distance-Apart/solution.java
+++ 358-Rearrange-String-k-Distance-Apart/solution.java
@@ -47,7 +47,7 @@ public class Solution {
                 map.put(ch, 1);
             }
         }
-        Queue<Node> q = new PriorityQueue<Node>(100, new Comparacter<Node>() {
+        Queue<Node> q = new PriorityQueue<Node>(100, new Comparator<Node>() {
             @Override
             public int compare(Node a, Node b) {
                 return b.count - a.count;
