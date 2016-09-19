52d1211517b2cf574117c75a09382d142fd11b31
stonebai <dairystone@gmail.com>
2016-09-18 17:44:10 -0700

    [295. Find Median from Data Stream][Runtime Error]committed by LeetCode Extension

--- 295-Find-Median-from-Data-Stream/solution.java
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -1,69 +1,27 @@
-class MedianFinder {
-
-    TreeMap<Integer, Integer> left = new TreeMap<Integer, Integer>();
-    TreeMap<Integer, Integer> right = new TreeMap<Integer, Integer>();
-    int lcount = 0;
-    int rcount = 0;
+public class MedianFinder {
+    
+    private Queue<Integer> minQ = new PriorityQueue<Integer>();
+    private Queue<Integer> maxQ = new PriorityQueue<Integer>(100, new Comparator<Integer>() {
+        @Override
+        public int compare(Integer a, Integer b) {
+            return b.compareTo(a);
+        }
+    });
     
     // Adds a number into the data structure.
     public void addNum(int num) {
-        if(lcount==0 && rcount==0) {
-            left.put(num, 1);
-            lcount++;
-        }
-        else {
-            Integer leftMax = left.lastKey();
-            if(num>leftMax) {
-                put(num, right);
-                rcount++;
-            }
-            else {
-                put(num, left);
-                lcount++;
-            }
-        }
-        while(lcount-rcount>1) {
-            Map.Entry<Integer, Integer> entry = left.lastEntry();
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if(value==1) left.remove(key);
-            else left.put(key, value - 1);
-            lcount--;
-            put(key, right);
-            rcount++;
-        }
-        while(rcount-lcount>1) {
-            Map.Entry<Integer, Integer> entry = right.firstEntry();
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if(value==1) right.remove(key);
-            else right.put(key, value - 1);
-            rcount--;
-            put(key, left);
-            lcount++;
-        }
-    }
-    
-    private void put(int num, TreeMap<Integer, Integer> map) {
-        if(map.containsKey(num)) {
-            int count = map.get(num);
-            map.put(num, count+1);
-        }
-        else {
-            map.put(num, 1);
+        maxQ.offer(num);
+        while (minQ.size() < maxQ.size()) {
+            minQ.offer(maxQ.poll());
         }
     }
 
     // Returns the median of current data stream
     public double findMedian() {
-        if(lcount>rcount) {
-            return (double)left.lastKey();
-        }
-        else if(rcount>lcount) {
-            return (double) right.firstKey();
-        }
-        else {
-            return (double)(left.lastKey() + right.firstKey()) / 2.0;
+        if (minQ.size() == maxQ.size()) {
+            return (minQ.peek() + maxQ.peek()) / 2.0;
+        } else {
+            return maxQ.peek();
         }
     }
 };
