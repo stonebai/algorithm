5c30bba8edf3ada85252662eb9249ffb948368ea
stonebai <dairystone@gmail.com>
2016-09-18 21:48:40 -0700

    [327. Count of Range Sum][Runtime Error]committed by LeetCode Extension

--- 327-Count-of-Range-Sum/solution.java
+++ 327-Count-of-Range-Sum/solution.java
@@ -1,24 +1,41 @@
 public class Solution {
     public int countRangeSum(int[] nums, int lower, int upper) {
+        int[] idx = new int[nums.length];
+        for (int i = 0;i < idx.length;i++) {
+            idx[i] = i;
+        }
+        idx = mergeSort(nums, idx, 0, nums.length - 1);
         int res = 0;
-        long prefixSum = 0;
-        TreeMap<Long, Integer> map = new TreeMap<Long, Integer>();
-        map.put((long) 0, 1);
-        for (int num : nums) {
-            prefixSum += num;
-            // cur - pre >= lower => pre <= cur - lower
-            // cur - pre <= upper => pre >= cur - upper
-            Map<Long, Integer> m = map.subMap(prefixSum - upper, true, prefixSum - lower, true);
-            if (m != null) {
-                for (Map.Entry<Long, Integer> entry : m.entrySet()) {
-                    res += entry.getValue();
-                }
-            }
-            if (map.containsKey(prefixSum)) {
-                map.put(prefixSum, map.get(prefixSum) + 1);
+        for (int i = 0;i < idx.length;i++) {
+            res = Math.max(res, i - idx[i]);
+        }
+        return res;
+    }
+    
+    private int[] mergeSort(int[] nums, int[] indexes, int lo, int hi) {
+        int[] res = new int[hi - lo + 1];
+        if (lo == hi) {
+            res[0] = indexes[lo];
+            return res;
+        }
+        int mid = lo + (hi - lo) / 2;
+        int[] left = mergeSort(nums, indexes, lo, mid);
+        int[] right = mergeSort(nums, indexes, mid + 1, hi);
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            int idL = left[i];
+            int idR = right[j];
+            if (nums[idL] <= nums[idR]) {
+                res[k++] = left[i++];
             } else {
-                map.put(prefixSum, 1);
+                res[k++] = right[j++];
             }
+        }
+        while (i < left.length) {
+            res[k++] = left[i++];
+        }
+        while (j < right.length) {
+            res[k++] = right[j++];
         }
         return res;
     }
