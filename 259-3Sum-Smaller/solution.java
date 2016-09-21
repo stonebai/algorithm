ca58a840e5ca00c50757ed219c1d2febbcdd9923
stonebai <dairystone@gmail.com>
2016-09-21 13:03:50 -0700

    [259. 3Sum Smaller][Accepted]committed by LeetCode Extension

--- 259-3Sum-Smaller/solution.java
+++ 259-3Sum-Smaller/solution.java
@@ -4,25 +4,14 @@ public class Solution {
             return 0;
         }
         int res = 0;
-        Arrays.sort(nums);
-        int i = 0;
-        while (i < nums.length - 2) {
-            int j = i + 1;
-            while (j < nums.length - 1) {
-                int k = j + 1;
-                while (k < nums.length) {
-                    int tmp = nums[i] + nums[j] + nums[k];
-                    if (tmp < target) {
+        for (int i = 0;i < nums.length - 2;i++) {
+            for (int j = i + 1;j < nums.length - 1;j++) {
+                for (int k = j + 1;k < nums.length;k++) {
+                    if (nums[i] + nums[j] + nums[k] < target) {
                         res++;
                     }
-                    k++;
                 }
-                j++;
             }
-            // while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-            //     i++;
-            // }
-            i++;
         }
         return res;
     }
