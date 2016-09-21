7583b1485d0a6ef4cdec4abe80843f424ba1553a
stonebai <dairystone@gmail.com>
2016-09-21 14:08:06 -0700

    [318. Maximum Product of Word Lengths][Accepted]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -16,8 +16,9 @@ public class Solution {
         int res = 0;
         for (int i = 0;i < nums.length;i++) {
             for (int j = i + 1;j < nums.length;j++) {
-                if (nums[i] & nums[j] == 0) {
+                if ((nums[i] & nums[j]) == 0) {
                     res = Math.max(res, words[i].length() * words[j].length());
+                    break;
                 }
             }
         }
