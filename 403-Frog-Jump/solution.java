41f4396779ed0d581410bee71e22d0dee405cfd5
stonebai <dairystone@gmail.com>
2016-09-21 11:38:58 -0700

    [403. Frog Jump][Accepted]committed by LeetCode Extension

--- 403-Frog-Jump/solution.java
+++ 403-Frog-Jump/solution.java
@@ -12,24 +12,26 @@ public class Solution {
         if (stones[1] != 1) {
             return false;
         }
-        boolean[] dp = new boolean[stones.length];
-        return helper(stones, 2, 2, dp);
+        boolean[][] dp = new boolean[stones.length][stones.length];
+        return helper(stones, 2, 1, dp);
     }
     
-    private helper(int[] stones, int pos, int jump, boolean[] dp) {
+    private boolean helper(int[] stones, int pos, int jump, boolean[][] dp) {
         if (pos >= stones.length) {
             return true;
         }
-        if (dp[pos]) {
+        if (dp[pos][jump]) {
             return false;
         }
-        dp[pos] = true;
+        dp[pos][jump] = true;
+        int lo = stones[pos - 1] + jump - 1;
+        int hi = stones[pos - 1] + jump + 1;
         boolean res = false;
         for (int i = pos;i < stones.length;i++) {
-            if (stones[i] > jump + 1) {
+            if (stones[i] > hi) {
                 break;
-            } else if (stones[i] >= jump - 1 && stones[i] <= jump + 1) {
-                if (helper(stones, i + 1, stones[i] - pos + 1, dp)) {
+            } else if (stones[i] <= hi && stones[i] >= lo) {
+                if (helper(stones, i + 1, stones[i] - stones[pos - 1], dp)) {
                     res = true;
                     break;
                 }
