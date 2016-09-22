b1a7825ea17f1b9dab8b608e4bbb4838c7e23ead
stonebai <dairystone@gmail.com>
2016-09-21 17:30:50 -0700

    [276. Paint Fence][Compile Error]committed by LeetCode Extension

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -3,17 +3,26 @@ public class Solution {
         if (n == 0 || k == 0) {
             return 0;
         }
-        return k * helper(n - 1, k, false);
+        int[][] dp = new int[n][2];
+        return k * helper(n - 1, k, false, dp);
     }
     
-    private int helper(int n, int k, boolean same) {
+    private int helper(int n, int k, boolean same, int[][] dp) {
         if (n == 0) {
             return 1;
         }
         if (same) {
-            return (k - 1) * helper(n - 1, k, false);
+            if (dp[n][0] != 0) {
+                return dp[n][0];
+            }
+            dp[n][0] = (k - 1) * helper(n - 1, k, false);
+            return dp[n][0];
         } else {
-            return (k - 1) * helper(n - 1, k, false) + helper(n - 1, k, true);
+            if (dp[n][1] != 0) {
+                return dp[n][1];
+            }
+            dp[n][1] = (k - 1) * helper(n - 1, k, false) + helper(n - 1, k, true);
+            return dp[n][1];
         }
     }
 }
