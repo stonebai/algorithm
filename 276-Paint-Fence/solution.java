96f303899b07cd0f968baa5c08f6a7207456b8ec
stonebai <dairystone@gmail.com>
2016-09-21 17:28:07 -0700

    [276. Paint Fence][Time Limit Exceeded]committed by LeetCode Extension

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -1,5 +1,19 @@
 public class Solution {
     public int numWays(int n, int k) {
-        return n * Math.pow(k, n - 1);
+        if (n == 0 || k == 0) {
+            return 0;
+        }
+        return k * helper(n - 1, k, false);
+    }
+    
+    private int helper(int n, int k, boolean same) {
+        if (n == 0) {
+            return 1;
+        }
+        if (same) {
+            return (k - 1) * helper(n - 1, k, false);
+        } else {
+            return (k - 1) * helper(n - 1, k, false) + helper(n - 1, k, true);
+        }
     }
 }
